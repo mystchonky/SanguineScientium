1b55b564cfb8ff03480d4db5af303931fb7b7130
luiq54 <dev.darshanphaldesai@gmail.com>
2022-12-31 13:35:45 +0530

    fix docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,11 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'luiq54', // Usually your GitHub org/user name.
-  projectName: 'BloodMagic-Docs', // Usually your repo name.
+  organizationName: 'luiq54',
+  projectName: 'BloodMagic-Docs',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -34,17 +31,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/luiq54/BloodMagic-Docs/edit/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -70,7 +58,7 @@ const config = {
             label: 'Guide',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/luiq54/BloodMagic-Docs',
             label: 'GitHub',
             position: 'right',
           },
