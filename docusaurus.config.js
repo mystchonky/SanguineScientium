82f22285864c7dc36bddff1883c493abe75ea1f9
luiq54 <dev.darshanphaldesai@gmail.com>
2022-12-31 13:28:27 +0530

    prep for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Blood Magic : Alchemical Wizardry',
   tagline: 'Gruesome? Probably. Worth it? Definitely!',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://luiq54.github.io/',
+  baseUrl: '/BloodMagic-Docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'luiq54', // Usually your GitHub org/user name.
+  projectName: 'BloodMagic-Docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
